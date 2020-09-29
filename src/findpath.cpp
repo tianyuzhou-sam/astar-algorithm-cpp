6e99385d6aa0e66f2a5dee1c500d65d7ff574f97
zehuilu <zehuilu@umich.edu>
2020-09-29 18:16:53 -0400

    update astar

---
+++ src/findpath.cpp
@@ -0,0 +1,197 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// STL A* Search implementation
+// (C)2001 Justin Heyes-Jones
+//
+// Finding a path on a simple grid maze
+// This shows how to do shortest path finding using A*
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include "stlastar.h"
+#include "MapSearchNode.h"
+#include "MapInfo.h"
+
+#define DEBUG_LISTS 0
+#define DEBUG_LIST_LENGTHS_ONLY 0
+
+
+// Main
+int main( int argc, char *argv[] )
+{
+
+	// std::cout << "STL A* Search implementation\n(C)2001 Justin Heyes-Jones\n";
+
+	// Our sample problem defines the world as a 2d array representing a terrain
+	// Each element contains an integer from 0 to 5 which indicates the cost 
+	// of travel across the terrain. Zero means the least possible difficulty 
+	// in travelling (think ice rink if you can skate) whilst 5 represents the 
+	// most difficult. 9 indicates that we cannot pass.
+
+	// Create an instance of the search class...
+
+	AStarSearch<MapSearchNode> astarsearch;
+
+	unsigned int SearchCount = 0;
+
+	const unsigned int NumSearches = 1;
+
+
+	std::vector<int> world_map = 
+	{
+	// 0001020304050607080910111213141516171819
+		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 00
+		1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,1,   // 01
+		1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 02
+		1,9,9,1,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 03
+		1,9,1,1,1,1,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 04
+		1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 05
+		1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 06
+		1,9,9,9,9,9,9,9,9,1,1,1,9,9,9,9,9,9,9,1,   // 07
+		1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 08
+		1,9,1,9,9,9,9,9,9,9,1,1,9,9,9,9,9,9,9,1,   // 09
+		1,9,1,1,1,1,9,1,1,9,1,1,1,1,1,1,1,1,1,1,   // 10
+		1,9,9,9,9,9,1,9,1,9,1,9,9,9,9,9,1,1,1,1,   // 11
+		1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 12
+		1,9,1,9,1,9,9,9,1,9,1,9,1,9,1,9,9,9,1,1,   // 13
+		1,9,1,1,1,1,9,9,1,9,1,9,1,1,1,1,9,9,1,1,   // 14
+		1,9,1,1,9,1,1,1,1,9,1,1,1,1,9,1,1,1,1,1,   // 15
+		1,9,9,9,9,1,1,1,1,1,1,9,9,9,9,1,1,1,1,1,   // 16
+		1,1,9,9,9,9,9,9,9,1,1,1,9,9,9,1,9,9,9,9,   // 17
+		1,9,1,1,1,1,1,1,1,1,1,9,1,1,1,1,1,1,1,1,   // 18
+		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,   // 19
+	};
+
+	int map_width = 20;
+	int map_height = 20;
+
+	struct MapInfo Map;
+	Map.world_map = world_map;
+	Map.map_width = map_width;
+	Map.map_height = map_height;
+
+
+	while(SearchCount < NumSearches)
+	{
+
+		// Create a start state
+		// int start_x = rand()%map_width;
+		// int start_y = rand()%map_height;
+
+		int start_x = 0;
+		int start_y = 0;
+		// MapSearchNode nodeStart;
+		MapSearchNode nodeStart = MapSearchNode(start_x, start_y, Map);
+
+		// Define the goal state
+		// int end_x = rand()%map_width;
+		// int end_y = rand()%map_height;
+
+		int end_x = 14;
+		int end_y = 10;
+		MapSearchNode nodeEnd(end_x, end_y, Map);
+
+		// Set Start and goal states
+		astarsearch.SetStartAndGoalStates( nodeStart, nodeEnd );
+
+		unsigned int SearchState;
+		unsigned int SearchSteps = 0;
+
+		do
+		{
+			SearchState = astarsearch.SearchStep();
+
+			SearchSteps++;
+
+	#if DEBUG_LISTS
+
+			std::cout << "Steps:" << SearchSteps << "\n";
+
+			int len = 0;
+
+			std::cout << "Open:\n";
+			MapSearchNode *p = astarsearch.GetOpenListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY			
+				((MapSearchNode *)p)->PrintNodeInfo();
+	#endif
+				p = astarsearch.GetOpenListNext();
+				
+			}
+
+			std::cout << "Open list has " << len << " nodes\n";
+
+			len = 0;
+
+			std::cout << "Closed:\n";
+			p = astarsearch.GetClosedListStart();
+			while( p )
+			{
+				len++;
+	#if !DEBUG_LIST_LENGTHS_ONLY			
+				p->PrintNodeInfo();
+	#endif			
+				p = astarsearch.GetClosedListNext();
+			}
+
+			std::cout << "Closed list has " << len << " nodes\n";
+	#endif
+
+		}
+		while( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SEARCHING );
+
+		if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_SUCCEEDED )
+		{
+			std::cout << "Search found goal state\n";
+
+				MapSearchNode *node = astarsearch.GetSolutionStart();
+
+	#if DISPLAY_SOLUTION
+				std::cout << "Displaying solution\n";
+	#endif
+				int steps = 0;
+
+				node->PrintNodeInfo();
+				for( ;; )
+				{
+					node = astarsearch.GetSolutionNext();
+
+					if( !node )
+					{
+						break;
+					}
+
+					node->PrintNodeInfo();
+					steps ++;
+				
+				};
+
+				std::cout << "Solution steps " << steps << endl;
+
+				// Once you're done with the solution you can free the nodes up
+				astarsearch.FreeSolutionNodes();
+
+	
+		}
+		else if( SearchState == AStarSearch<MapSearchNode>::SEARCH_STATE_FAILED ) 
+		{
+			std::cout << "Search terminated. Did not find goal state\n";
+		
+		}
+
+		// Display the number of loops the search went through
+		std::cout << "SearchSteps : " << SearchSteps << "\n";
+
+		SearchCount ++;
+
+		astarsearch.EnsureMemoryFreed();
+	}
+	
+	return 0;
+}
