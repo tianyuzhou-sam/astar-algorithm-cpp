5c348999763862acc251ed3cb1cb98883dc260ba
zehuilu <zehuilu@umich.edu>
2020-10-04 23:17:52 -0400

    update plor

--- test/test_plot.py
+++ test/test_plot.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd()+'/build')
 sys.path.append(os.getcwd()+'/src')
 import AStarPython
 import numpy as np
+import time
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
@@ -19,7 +20,7 @@ if __name__ == "__main__":
 	# create a simulator
 	Simulator = helper.Simulator(map_width_meter, map_width_meter, map_resolution)
 	# number of obstacles
-	num_obs = 20
+	num_obs = 50
 	# generate random obstacles
 	Simulator.generate_random_obs(num_obs)
 
@@ -30,10 +31,15 @@ if __name__ == "__main__":
 	# convert 2D numpy array to 1D list
 	world_map = Simulator.map_array.flatten().tolist()
 	# solve it
+	t0 = time.time()
 	path_full, path_short = AStarPython.FindPath(start, end, world_map, Simulator.map_width, Simulator.map_height)
+	t1 = time.time()
 
 	num_steps = len(path_full)/2 - 1
 	print(num_steps)
+
+	print("Time used is [sec]")
+	print(t1-t0)
 
 	# visualization
 	if len(path_full) > 0:
