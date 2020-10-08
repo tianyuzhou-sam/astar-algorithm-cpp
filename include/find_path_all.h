0d95b2321fa52e2bf0690b38246e2e9ff5603fef
zehuilu <zehuilu@umich.edu>
2020-10-07 22:43:30 -0400

    update header files

---
+++ include/find_path_all.h
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// STL A* Search implementation
+// (C)2001 Justin Heyes-Jones
+//
+// Finding a path on a simple grid maze
+// This shows how to do shortest path finding using A*
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <math.h>
+#include "stlastar.h"
+#include "MapSearchNode.h"
+#include "MapInfo.h"
+#include "find_path.h"
+
+
+inline std::vector<int> find_path_all(
+	std::vector<int> start,
+	std::vector<int> end,
+	const MapInfo &Map)
+{
+
+
+}
--- test/test_combination.cpp
+++ test/test_combination.cpp
@@ -9,48 +9,22 @@ Oct. 07, 2020
 #include <string>
 #include <chrono>
 #include <vector>
-
-
-inline std::vector<int> combination(int N, int K)
-{
-    std::vector<int> result;
-    int idx = 0;
-
-    std::string bitmask(K, 1); // K leading 1's
-    bitmask.resize(N, 0); // N-K trailing 0's
-
-    // print integers and permute bitmask
-    do {
-        for (int i = 0; i < N; ++i) // [0..N-1] integers
-        {
-            if (bitmask[i])
-            {
-                std::cout << " " << i;
-                result.push_back(i);
-            }
-        }
-        std::cout << std::endl;
-
-        idx++;
-    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
-
-    return result;
-}
+#include "get_combination.h"
 
 
 int main()
 {
     auto t0 = std::chrono::high_resolution_clock::now();
 
-    std::vector<int> result = combination(10, 2);
+    std::vector<int> result = get_combination(10, 2);
 
     auto t1 = std::chrono::high_resolution_clock::now();
     auto time_used = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
 
     std::cout << time_used << "microseconds" << std::endl;
-    for (unsigned long i = 0; i < result.size(); i++)
+    for (unsigned long i = 0; i < result.size(); i = i + 2)
     {
-        std::cout << result[i] << std::endl;
+        std::cout << result[i] << ", " << result[i+1] << std::endl;
     }
 
 }
